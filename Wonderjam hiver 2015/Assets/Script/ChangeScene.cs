c7c1d453e0d42bd6f702c7367b4462689d865533
Francois Pre <francois_pre@hotmail.com>
2015-02-28 16:10:19 -0500

    MainMenu

---
+++ Wonderjam hiver 2015/Assets/Script/ChangeScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public string levelName;
+
+	public void LoadLevel(string levelName){
+
+		Application.LoadLevel (levelName);
+	}
+
+
+}
