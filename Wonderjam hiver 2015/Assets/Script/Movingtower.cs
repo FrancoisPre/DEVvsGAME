f31572ef4072341f8cd144cf92020b283051aa8d
Kupos <samuel_872@hotmail.com>
2015-02-28 00:49:51 -0500

    Ze mousedrag scrip is done
    +7

--- Wonderjam hiver 2015/Assets/Script/Movingtower.cs
+++ Wonderjam hiver 2015/Assets/Script/Movingtower.cs
@@ -1,36 +1,42 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
 
 public class Movingtower : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
-
-
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-
-
+		
+		
+		
 		if(Input.GetMouseButton(0)){
 			Debug.Log("Held");
-
+			
 			Vector3 pos = Input.mousePosition;
+			//pos.z = 1;
+			pos.z = Camera.main.transform.position.z;
+			
 			pos = Camera.main.ScreenToWorldPoint(pos);
-
 			Debug.Log(pos);
-			transform.position = pos;
-			//			transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
-
+			pos.x = Camera.main.transform.position.x-pos.x;
+			
+			//Le 8 ici risque d'avoir besoin d'etre changé, il varie selon la caméra
+			pos.y = 6+Camera.main.transform.position.y-pos.y;
+			transform.position = Vector2.Lerp(transform.position, pos, 0.9f);
+			
 		}
 		else{
 			Debug.Log("Not held");
 		}
-
-
 		
-
+		
+		
+		
 	}
 }
