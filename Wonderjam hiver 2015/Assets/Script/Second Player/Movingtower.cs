289c923d1e43b25cc54a3baa0616472d95ba2ad0
Kupos <samuel_872@hotmail.com>
2015-02-28 02:13:35 -0500

    Second player moving towers
    On/off buttons complete

---
+++ Wonderjam hiver 2015/Assets/Script/Second Player/Movingtower.cs
@@ -0,0 +1,45 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Movingtower : MonoBehaviour {
+	public bool ready;
+
+	// Use this for initialization
+	void Start () {
+		ready = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		if (ready) {
+			if (Input.GetMouseButton (1)) {
+				Debug.Log ("Held");
+				
+				Vector3 pos = Input.mousePosition;
+				//pos.z = 1;
+				pos.z = Camera.main.transform.position.z;
+				
+				pos = Camera.main.ScreenToWorldPoint (pos);
+				Debug.Log (pos);
+				pos.x = Camera.main.transform.position.x - pos.x;
+				
+				//Le 8 ici risque d'avoir besoin d'etre changé, il varie selon la caméra
+				pos.y = 6 + Camera.main.transform.position.y - pos.y;
+				transform.position = Vector2.Lerp (transform.position, pos, 0.9f);
+				
+			} else {
+				Debug.Log ("Not held");
+			}
+		}
+	}
+
+	public void changeReady () { 
+		ready = !ready; 
+		
+		
+	}
+}
