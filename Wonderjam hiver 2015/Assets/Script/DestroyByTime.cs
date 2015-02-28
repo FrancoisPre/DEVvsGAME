285c70edb720ab65857c799704964205445c2c4c
Francois Pre <francois_pre@hotmail.com>
2015-02-28 01:45:21 -0500

    stff

---
+++ Wonderjam hiver 2015/Assets/Script/DestroyByTime.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+	public float timer;
+
+	// Use this for initialization
+	void Start () {
+
+		Destroy (gameObject, timer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
