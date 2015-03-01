8d51ba61ddbb3e269f81e06dda40af15c3c7583c
Sefa <sefa65130@gmail.com>
2015-03-01 06:53:04 -0500

    Increase jump responsivness

--- Wonderjam hiver 2015/Assets/Script/PlayerController.cs
+++ Wonderjam hiver 2015/Assets/Script/PlayerController.cs
@@ -33,6 +33,9 @@ public class PlayerController : MonoBehaviour {
 
 	void OnDrawGizmos(){
 		Gizmos.DrawLine(rigidbody2D.position, rigidbody2D.position  + new Vector2(0.0f,-mindistjump));
+		Gizmos.DrawLine(rigidbody2D.position + new Vector2(1.0f,0.0f), rigidbody2D.position  + new Vector2(1.0f,-mindistjump));
+		Gizmos.DrawLine(rigidbody2D.position + new Vector2(-1.0f,0.0f), rigidbody2D.position  + new Vector2(-1.0f,-mindistjump));
+
 	}
    
 
@@ -58,13 +61,23 @@ public class PlayerController : MonoBehaviour {
 			rigidbody2D.rotation = 0.0f;
 		}
 		else{
-			RaycastHit2D hit;
+			RaycastHit2D hit, hitleft, hitright;
 			Vector2 rayCastPosition = new Vector2(rigidbody2D.position.x,rigidbody2D.position.y-raycastlenght);
+			Vector2 rayCastLeft = new Vector2(rigidbody2D.position.x-1,rigidbody2D.position.y-raycastlenght);
+			Vector2 rayCastRight = new Vector2(rigidbody2D.position.x+1,rigidbody2D.position.y-raycastlenght);
+			Vector2 leftPos = rigidbody2D.position-new Vector2(-1.0f,0.0f);
+			Vector2 rightPos =rigidbody2D.position-new Vector2(+1.0f,0.0f);
 			Vector2 offset = rayCastPosition - rigidbody2D.position;
+			Vector2 offsetLeft = rayCastLeft - leftPos;
+			Vector2 offsetRight = rayCastRight -rightPos;
 			hit = Physics2D.Raycast(rigidbody2D.position,offset.normalized,offset.sqrMagnitude,layermask);
+			hitleft = Physics2D.Raycast(leftPos,offsetLeft.normalized, offsetLeft.sqrMagnitude, layermask);
+			hitright = Physics2D.Raycast(rightPos,offsetRight.normalized, offsetRight.sqrMagnitude, layermask);
 			float tmp = hit.distance;
+			float tmpLeft = hitleft.distance;
+			float tmpRight = hitright.distance;
 			//Debug.Log(tmp);
-			if (tmp!=0 && tmp<mindistjump){
+				if ((tmp!=0 && tmp<mindistjump)||(tmpLeft!=0 && tmpLeft<mindistjump)||(tmpRight!=0 && tmpRight<mindistjump)){
 				jumpable=true;
 				doubleJump=true;
 			}
