ed8ec429a54ea80904a111292138e5a8633586a9
Sefa <sefa65130@gmail.com>
2015-02-28 19:13:31 -0500

    fix empilement plateforme

--- Wonderjam hiver 2015/Assets/Script/PlatformManager.cs
+++ Wonderjam hiver 2015/Assets/Script/PlatformManager.cs
@@ -8,15 +8,14 @@ public class PlatformManager : MonoBehaviour {
 	public int numberOfObjects;
 	public float minGap,maxGap,minY,maxY;
 	public Vector3 startPosition;
-	private Vector3 nextPosition1, nextPosition2;
+	private Vector3 nextPosition1;
 	private Vector3 exitPosition;
 	public Transform basicPlatform;
 	public int nbElemPetit,nbElemMoyen,nbElemGrand;
 	public Transform exit;
 	// Use this for initialization
 	void Start () {
-		nextPosition1= new Vector3(0.0f,0.0f,0.0f);
-		nextPosition2=startPosition;
+		nextPosition1= startPosition;
 		for (int i =0; i < numberOfObjects; i++)
 			spawnPlateform();
 		spawnExit();
@@ -49,35 +48,13 @@ public class PlatformManager : MonoBehaviour {
 				Instantiate(grand,nextPosition1, Quaternion.identity);
 				nb1 = nbElemGrand;
 			}
-			c = Random.Range(0,2);
-			int nb2;
-			if (c%3 == 0){
-				Instantiate(petit,nextPosition2, Quaternion.identity);
-				nb2 = nbElemPetit;
-			}
-			else if (c%3 ==1){
-				Instantiate(moyen,nextPosition2, Quaternion.identity);
-				nb2 = nbElemMoyen;
-			}
-			else{
-				Instantiate(grand,nextPosition2, Quaternion.identity);
-				nb2 = nbElemGrand;
-			}
-			c = Random.Range(0,1);
-			float size1,size2;
+			float size1;
 			size1 = nb1* basicPlatform.renderer.bounds.size.x;
-			size2 = nb2* basicPlatform.renderer.bounds.size.x;
-			if (c==0){
-				exitPosition = nextPosition1;
-				nextPosition2= nextPosition1 + new Vector3(Random.Range(minGap,maxGap) + size1,Random.Range(minY,maxY),0.0f);
-				nextPosition1= nextPosition1 + new Vector3(Random.Range(minGap,maxGap) + size1,Random.Range(minY,maxY),0.0f);
-			}
-			else{
-				exitPosition = nextPosition2;
-				nextPosition1= nextPosition2 + new Vector3(Random.Range(minGap,maxGap) + size2,Random.Range(minY,maxY),0.0f);
-				nextPosition2= nextPosition2 + new Vector3(Random.Range(minGap,maxGap) + size2,Random.Range(minY,maxY),0.0f);
-			}
+			exitPosition = nextPosition1;
+			nextPosition1= nextPosition1 + new Vector3(Random.Range(minGap,maxGap) + size1,Random.Range(minY,maxY),0.0f);
+				
 		}
 	}
 }
+
 
