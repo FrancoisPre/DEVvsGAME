bc64e80eee3539884320efd4526f5395a2de26d0
MarcAndreFL <marc1999_8@hotmail.com>
2015-02-28 21:44:37 -0500

    ENEMY PREFAB ET ENEMY CONTROLLER QUI MARCHE
    QUI MARCHE

--- Wonderjam hiver 2015/Assets/Script/EnemyController.cs
+++ Wonderjam hiver 2015/Assets/Script/EnemyController.cs
@@ -5,6 +5,7 @@ public class EnemyController : MonoBehaviour {
 	
 	public float speed;
 	public float rayCastSide;//ICI JE CRÉE LES LAYERS NÉCESSAIRES AFIN D'ÉVITER QUE L'ENEMY PARTE DE LA PLATEFORME
+	public float horizontalCastSide;
 	public int maskPlatform; //INDICE DU LAYER 
 	private Vector2 velocity;
 	private bool isSnap=false;
@@ -35,16 +36,23 @@ public class EnemyController : MonoBehaviour {
 	{
 		Vector2 leftRayCastPosition = new Vector2 (rigidbody2D.position.x - rayCastSide, rigidbody2D.position.y - rayCastSide);
 		
-		Gizmos.DrawLine (new Vector3(leftRayCastPosition.x,leftRayCastPosition.y,0), new Vector3(rigidbody2D.position.x,rigidbody2D.position.y,0));
+		Gizmos.DrawLine (new Vector3 (leftRayCastPosition.x, leftRayCastPosition.y, 0), new Vector3 (rigidbody2D.position.x, rigidbody2D.position.y, 0));
 		
 		Vector2 rightRayCastPosition = new Vector2 (rigidbody2D.position.x + rayCastSide, rigidbody2D.position.y - rayCastSide);
 		
-		Gizmos.DrawLine (new Vector3(rightRayCastPosition.x,rightRayCastPosition.y,0), new Vector3(rigidbody2D.position.x,rigidbody2D.position.y,0));
+		Gizmos.DrawLine (new Vector3 (rightRayCastPosition.x, rightRayCastPosition.y, 0), new Vector3 (rigidbody2D.position.x, rigidbody2D.position.y, 0));
+
+		Vector2 leftHorizontalCastPosition = new Vector2 (rigidbody2D.position.x - horizontalCastSide, rigidbody2D.position.y);
+		Gizmos.DrawLine (new Vector3 (leftHorizontalCastPosition.x, leftHorizontalCastPosition.y, 0), new Vector3 (rigidbody2D.position.x, rigidbody2D.position.y, 0));
+
+		Vector2  rightHorizontalCastPosition = new Vector2 (rigidbody2D.position.x + horizontalCastSide, rigidbody2D.position.y);
+		Gizmos.DrawLine (new Vector3(rightHorizontalCastPosition.x,rightHorizontalCastPosition.y,0), new Vector3(rigidbody2D.position.x,rigidbody2D.position.y,0));
 	}
-	
 	void FixedUpdate(){
 		RaycastHit2D hitLeft;
 		RaycastHit2D hitRight;
+		RaycastHit2D hitHorizontalLeft;
+		RaycastHit2D hitHorizontalRight;
 		
 		
 		
@@ -53,21 +61,54 @@ public class EnemyController : MonoBehaviour {
 		Vector2 leftRayCastPosition = new Vector2 (rigidbody2D.position.x - rayCastSide, rigidbody2D.position.y - rayCastSide);
 		var leftOffset = leftRayCastPosition - rigidbody2D.position;
 		hitLeft = Physics2D.Raycast (rigidbody2D.position, leftOffset.normalized, leftOffset.sqrMagnitude, finalMask);
+
 		//On vérifie en bas a droite de l'enemi
 		Vector2 rightRayCastPosition = new Vector2 (rigidbody2D.position.x + rayCastSide, rigidbody2D.position.y - rayCastSide);
 		var rightOffset = rightRayCastPosition - rigidbody2D.position;
 		hitRight = Physics2D.Raycast(rigidbody2D.position, rightOffset.normalized, rightOffset.sqrMagnitude, finalMask);
 		
-		
-		
-		
+
+
+		//LES HORIZONTALES RAYCAST
+
+
+		if (rigidbody2D.velocity.x < 0 && (int) rigidbody2D.velocity.y==0) {
+			Vector2  leftHorizontalCastPosition = new Vector2 (rigidbody2D.position.x - horizontalCastSide, rigidbody2D.position.y);
+			Debug.Log (leftHorizontalCastPosition);
+			Vector2 leftHorizontalOffset = leftHorizontalCastPosition - rigidbody2D.position;
+			Debug.Log (leftHorizontalOffset);
+			hitHorizontalLeft = Physics2D.Raycast (rigidbody2D.position, leftHorizontalOffset.normalized, leftHorizontalOffset.sqrMagnitude, finalMask);
+
+			if (hitHorizontalLeft.collider != null) {
+				Debug.Log("TAPPE PLATEFORME A L' HORIZONTALE VERS GAUCHE");
+				rigidbody2D.velocity = rigidbody2D.velocity * (-1.0f);
+			}
+		} 
+
+
+		else if(rigidbody2D.velocity.x>=0 &&(int)rigidbody2D.velocity.y==0)
+		{
+			Vector2  rightHorizontalCast = new Vector2 (rigidbody2D.position.x + horizontalCastSide, rigidbody2D.position.y);
+			Debug.Log (rightHorizontalCast);
+			Vector2 rightHorizontalOffset = rightHorizontalCast - rigidbody2D.position;
+			Debug.Log (rightHorizontalOffset);
+			hitHorizontalRight = Physics2D.Raycast (rigidbody2D.position, rightHorizontalOffset.normalized, rightHorizontalOffset.sqrMagnitude, finalMask);
+
+			if (hitHorizontalRight.collider != null) {
+				Debug.Log("TAPPE PLATEFORME A L' HORIZONTALE VERS DROTIE");
+				rigidbody2D.velocity = rigidbody2D.velocity * (-1.0f);
+			}
+		}
+
+
+
 		// SI LE RAYCAST GAUCHE N'ENTRE PAS EN COLLISION AVEC UNE PLATEFORME
-		if (hitLeft.collider == null) {
+		if ((hitLeft.collider == null) ) {
 			Debug.Log ("rien a gauche");
 			rigidbody2D.velocity = rigidbody2D.velocity * (-1.0f);
 		}
 		// SI LE RAYCAST DROITE N'ENTRE PAS EN COLLISION AVEC UNE PLATEFORME
-		if (hitRight.collider == null) {
+		if ((hitRight.collider == null)) {
 			Debug.Log ("rien a droite");
 			rigidbody2D.velocity = rigidbody2D.velocity * (-1.0f);
 		}
